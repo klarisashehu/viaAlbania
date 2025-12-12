068cdd7fc02f26b5941b6da9ee2fe46c535229ef
Klarisa <klarisashehu12@gmail.com>
2025-12-12 21:00:45 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/viaAlbania/viaAlbania/ViaAlbaniaApplication.java
@@ -0,0 +1,13 @@
+package com.viaAlbania.viaAlbania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaAlbaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaAlbaniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viaAlbania/viaAlbania/ViaAlbaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viaAlbania.viaAlbania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaAlbaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
